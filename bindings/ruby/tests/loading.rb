e979f7d481dc5cbebd688262877760094e6079ca
Klaus Kämpf <kkaempf@suse.de>
2011-08-05 09:19:58 +0200

    Remove debug code

--- bindings/ruby/tests/loading.rb
+++ bindings/ruby/tests/loading.rb
@@ -3,11 +3,6 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
 # Test loading of the bindings
 #
 
-File.open("/tmp/output", "w") { |f|
-  f.puts "PWD: #{Dir.pwd}"
-}
-
-
 # test loading of extension
 require 'test/unit'
 
